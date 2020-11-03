024a542f5e90e14f56f7ec40b77a08d0df13be94
avpimblesr <allan.pimble@gmail.com>
2020-11-02 22:41:24 -0500

    Removed a file that I was using for a scratch pad.

--- help.txt.js
+++
@@ -1,9 +0,0 @@
-/*
-  Methods	Urls	Actions
-  GET	/customers	get all Customers
-  GET	/customers/42	get Customer with id=42
-  POST	/customers	add new Customer
-  PUT	/customers/42	update Customer with id=42
-  DELETE	/customers/42	remove Customer with id=42
-  DELETE	/customers	remove all Customers
-*/
